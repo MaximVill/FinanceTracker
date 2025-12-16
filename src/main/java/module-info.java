10e95afccc39f99b5aa6fa601d76bac11203a745
abramov222 <ilyabramov22222@gmail.com>
2025-12-16 20:25:53 +0700

    feat(ui): add FXML interface files (registration and main screen)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,11 @@ module org.example.financetracker {
     requires com.h2database;
     requires org.slf4j;
     requires java.net.http;
-
     opens org.example.financetracker.ui to javafx.fxml;
+    opens org.example.financetracker.model to javafx.base;
     exports org.example.financetracker;
+    exports org.example.financetracker.ui;
+    exports org.example.financetracker.model;
+    exports org.example.financetracker.service;
+    exports org.example.financetracker.db;
 }
