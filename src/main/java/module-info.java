a9e18f5fd679d8c2f698fa7fcc6b938432bc03d1
Maxim Sergeevich <flik883@gmail.com>
2025-12-17 17:35:12 +0700

    feat: transition to a new API with ruble support and simplified data storage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,10 @@ module org.example.financetracker {
     requires com.h2database;
     requires org.slf4j;
     requires java.net.http;
+    requires com.fasterxml.jackson.databind;
+
     opens org.example.financetracker.ui to javafx.fxml;
-    opens org.example.financetracker.model to javafx.base;
+    opens org.example.financetracker.model to javafx.base, com.fasterxml.jackson.databind;
     exports org.example.financetracker;
     exports org.example.financetracker.ui;
     exports org.example.financetracker.model;
